603496be1133b5c5cbb81b99f4056775e320002e
Jx <jebux25@hotmail.com>
2025-08-28 20:33:09 -0500

    Agregando usuario

--- lib/pages/myhome_page.dart
+++ lib/pages/myhome_page.dart
@@ -1,29 +1,29 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_profile/pages/profile_page.dart';
-
+ 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Home',
-          style: TextStyle(fontSize: 24),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => const Profile()),
-            );
-          },
-          child: const Text('Siguiente'),
-        ),
-      ],
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const Profile()),
+                );
+              },
+              child: const Text('Siguiente'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,13 +1,94 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_profile/models/user_model.dart';
+ 
 class Profile extends StatelessWidget {
   const Profile({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Profile')),
-      body: const Center(child: Text('Welcome!')),
+      body: Container(
+        padding: const EdgeInsets.all(16), // optional for spacing
+        child: Column(
+          children: const [
+            Text(
+              'Hello, this is your profile!',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20), // spacing
+            CircleAvatar(
+              backgroundColor: Color.fromARGB(255, 133, 77, 64),
+              radius: 30, // optional, to control the size
+              child: Icon(
+                Icons.person,
+                color: Colors.white,
+                size: 30,
+              )),
+            Column(
+              children: [
+                TableExample(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+ 
+class TableExample extends StatelessWidget {
+  const TableExample({super.key});
+ 
+  @override
+  Widget build(BuildContext context) {
+ 
+    UserModel  _usuario = UserModel(name: 'Jesús', age: 25, category: "Mayor de edad");
+ 
+    return Table(
+      border: TableBorder.all(),
+      columnWidths: {
+        0: FixedColumnWidth(100),
+        1: FlexColumnWidth(),
+      },
+      children: [
+        TableRow(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Nombre", style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("${_usuario.name}"),
+            ),
+          ],
+        ),
+         TableRow(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Edad", style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("${_usuario.age}"),
+            ),
+          ],
+        ),
+        TableRow(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Categoria", style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("${_usuario.category}"),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
